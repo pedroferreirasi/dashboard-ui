a09517050b5c6b6a38ad08d68086a44fd748993f
Pedro Ferreira <pedro.ferreira@dedalus.com>
2022-08-25 18:30:31 -0300

    estavel

--- src/app/data/model/task.model.ts
+++ src/app/data/model/task.model.ts
@@ -1,16 +1,24 @@
 export interface TaskModel {
-  id: number,
-  assigner: number,
-  project_id: number,
-  section_id: number,
-  order: number,
-  content: String,
-  description: String,
-  completed: boolean,
-  label_ids: String[],
-  priority: number,
-  comment_count: number,
-  creator: number,
-  created: String,
-  url: String
+  id: number;
+  assigner: number;
+  project_id: number;
+  section_id: number;
+  order: number;
+  content: string;
+  description: string;
+  completed: boolean;
+  label_ids: number[];
+  priority: number;
+  comment_count: number;
+  creator: number;
+  created: Date;
+  due: Due;
+  url: string;
+}
+
+export interface Due {
+  date: string;
+  string: string;
+  lang: string;
+  recurring: boolean;
 }
