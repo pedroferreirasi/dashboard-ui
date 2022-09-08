4491d6db865fa48028d546a9d69cc975c53f3607
Pedro Ferreira <pedro.ferreira@dedalus.com>
2022-09-08 17:50:11 -0300

    correcao da arvore de estrutura

--- src/app/data/services/task.service.ts
+++ src/app/data/services/task.service.ts
@@ -1,7 +1,6 @@
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { TaskModel } from "../../data/domain/entities/task.model";
 
 @Injectable({
@@ -18,52 +17,7 @@ export class TodoistService {
     headers: new HttpHeaders({"Authorization": "Bearer e79597ab575d629cc9553a515491e022ed52d04f"})
   }
 
-    // Manipulação de erros
-    handleError(error: HttpErrorResponse) {
-      let errorMessage = '';
-      if (error.error instanceof ErrorEvent) {
-        // Erro ocorreu no lado do client
-        errorMessage = error.error.message;
-      } else {
-        // Erro ocorreu no lado do servidor
-        errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
-      }
-      console.log(errorMessage);
-      return throwError(errorMessage);
-    };
-
   getAllTasks(): Observable<TaskModel[]> {
-    return this.http.get<TaskModel[]>(this.url, this.httpOptions)
-    .pipe(
-      retry(2),
-      catchError(this.handleError))
+    return this.http.get<TaskModel[]>(this.url, this.httpOptions);
   }
-
-  /*getHeaders(): HttpHeaders {
-    let headers: HttpHeaders = new HttpHeaders();
-    headers = headers.append('Error-Policy', 'ignore');
-    const token = 'Bearer e79597ab575d629cc9553a515491e022ed52d04f';
-    if (token) {
-      headers = headers.append('Authorization', token);
-    }
-    return headers;
-  }
-
-  getAllTasks(): Observable<String {
-    return this.http.get<String>(
-      this.getApiUrl('https://api.todoist.com/rest/v1/tasks'),
-      {
-        headers: this.getHeaders()
-      }
-    );
-  }
-
-  getApiUrl(uriSpecific?: string): string {
-    const uri: string = uriSpecific || this.apiUri;
-    return `${this.getBaseUrl()}${uri}`;
-  }
-
-  getBaseUrl(): string {
-    return this.dngConfigService.current.baseURL;
-  }*/
 }
